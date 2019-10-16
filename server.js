9f6dc7cd864d2be86529317d0e5984b8f62203a0
TANTANMOY <tanmoymandal457@gmail.com>
2019-10-17 00:54:59 +0530

    upto user sign up

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+//Body parser middleware
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
 
 //DB config
 
@@ -14,6 +28,11 @@ mongoose.connect(db)
 
 
 app.get('/', (req, res) => res.send('hello'));
+
+//Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
