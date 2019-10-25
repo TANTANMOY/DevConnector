30183550a38a69b9343fec7c1b13a857051664f8
TANTANMOY <tanmoymandal457@gmail.com>
2019-10-25 13:01:56 +0530

    create posts and get single post with id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+//Post model
+const Post = require('../../models/Post');
+//validation
+const validatePostInput = require('../../validation/post');
+
 
 // @route GET api/posts/test
 // @desc Tests post route
@@ -8,5 +17,60 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg: "Posts works"
 }));
+// @route GET api/posts
+// @desc  GET Post
+// @access public
+
+
+router.get('/', (req, res) => {
+    Post.find()
+        .sort({
+            date: -1
+        })
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({
+            nopostsfound: 'No posts found '
+        }));
+});
+
+// @route GET api/posts/:id
+// @desc  GET Post by id
+// @access public
+
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({
+            nopostfound: 'No post found with that Id'
+        }));
+});
+
+
+
+// @route POST api/posts
+// @desc  Create Post
+// @access private
+
+router.post('/', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validatePostInput(req.body);
+    //check validation
+    if (!isValid) {
+        //if any errors, send 400 with errors object
+        return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+});
 
 module.exports = router;
