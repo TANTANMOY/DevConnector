04100efb60d77d8185ae453e2b7bf19411adadfa
TANTANMOY <tanmoymandal457@gmail.com>
2019-11-04 10:25:40 +0530

    add education

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,7 +17,7 @@ import PrivateRoute from './common/PrivateRoute';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
-
+import AddEducation from './components/add-credentials/AddEducation';
 
 
 //check for token
@@ -64,6 +64,9 @@ function App() {
           </Switch>
           <Switch>
           <PrivateRoute exact path="/add-experience" component={AddExperience}/>
+          </Switch>
+          <Switch>
+          <PrivateRoute exact path="/add-education" component={AddEducation}/>
           </Switch>
         </div>
 
