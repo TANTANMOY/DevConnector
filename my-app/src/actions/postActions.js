de38bf3135e0e7220844639da530c8ad1bddb6d0
TANTANMOY <tanmoymandal457@gmail.com>
2019-11-09 14:27:56 +0530

    single post display

--- my-app/src/actions/postActions.js
+++ my-app/src/actions/postActions.js
@@ -4,13 +4,16 @@ import {
     ADD_POST,
     GET_ERRORS,
     GET_POSTS,
-    POST_LOADING
+    POST_LOADING,
+    DELETE_POST,
+    GET_POST
 }
 from './types';
 
 //Add Post
 
 export const addPost = (postData) =>dispatch => {
+    
     axios.post('/api/posts',postData)
     .then(res => 
         dispatch({
@@ -28,7 +31,7 @@ export const addPost = (postData) =>dispatch => {
 
 
 
-//GET Post
+//GET Posts
 
 export const getPosts = () =>dispatch => {
     dispatch(setPostLoading());
@@ -45,6 +48,77 @@ export const getPosts = () =>dispatch => {
             })
             );
 }
+
+
+//GET Post
+
+export const getPost = (id) =>dispatch => {
+    dispatch(setPostLoading());
+    axios.get(`/api/posts/${id}`)
+    .then(res => 
+        dispatch({
+            type: GET_POST,
+            payload: res.data
+        }))
+        .catch(err => 
+            dispatch({
+                type: GET_POST,
+                payload: null
+            })
+            );
+}
+
+//delete post
+
+
+export const deletePost = (id) =>dispatch => {
+    axios.delete(`/api/posts/${id}`)
+    .then(res => 
+        dispatch({
+            type: DELETE_POST,
+            payload: id
+        }))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+            );
+}
+
+//add like
+
+   
+export const addLike = (id) =>dispatch => {
+    axios.post(`/api/posts/like/${id}`)
+    .then(res => 
+        dispatch(getPosts()))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+            );
+}
+
+//remove like
+
+
+export const removeLike = (id) =>dispatch => {
+    axios.post(`/api/posts/unlike/${id}`)
+    .then(res => 
+        dispatch(getPosts()))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+            );
+}
+
+
+
+
 
 //set loading state
 
--- my-app/src/components/posts/PostItem.js
+++ my-app/src/components/posts/PostItem.js
@@ -3,14 +3,29 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
-
+import {deletePost,addLike,removeLike} from '../../actions/postActions';
 
  class PostItem extends Component {
      onDeleteClick(id){
-console.log(id);
+      this.props.deletePost(id);
+     }
+     onLikeClick(id){
+this.props.addLike(id);
+     }
+     onUnLikeClick(id){
+      this.props.removeLike(id);
+     }
+     findUserLike(likes){
+       const {auth} = this.props;
+       if(likes.filter(like => like.user === auth.user.id).length >0 ){
+         return true;
+       }
+       else {
+         return false;
+       }
      }
     render() {
-        const { post,auth } =this.props;
+        const { post,auth,showActions } =this.props;
 
         return (
            
@@ -29,11 +44,14 @@ console.log(id);
                   <p className="lead">
                       {post.text}
                   </p>
-                  <button type="button" className="btn btn-light mr-1">
-                    <i className="text-info fas fa-thumbs-up"></i>
+               {showActions ? (<span>
+                <button onClick={this.onLikeClick.bind(this,post._id)} type="button" className="btn btn-light mr-1">
+                    <i className={classnames('fas fa-thumbs-up', {
+                      'text-info': this.findUserLike(post.likes)
+                    })} />
                     <span className="badge badge-light">{post.likes.length}</span>
                   </button>
-                  <button type="button" className="btn btn-light mr-1">
+                  <button onClick={this.onUnLikeClick.bind(this,post._id)} type="button" className="btn btn-light mr-1">
                     <i className="text-secondary fas fa-thumbs-down"></i>
                   </button>
                   <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
@@ -47,6 +65,7 @@ console.log(id);
               )
               :null
               }
+               </span>) : null }
                 </div>
               </div>
             </div>
@@ -54,11 +73,18 @@ console.log(id);
         )
     }
 }
+PostItem.defaultProps = {
+  showActions: true
+}
+
 PostItem.propTypes = {
+  deletePost: PropTypes.func.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired
 }
 const mapStateToProps = state => ({
    auth: state.auth 
 });
-export default connect(mapStateToProps)(PostItem);
+export default connect(mapStateToProps, {deletePost,addLike,removeLike})(PostItem);
